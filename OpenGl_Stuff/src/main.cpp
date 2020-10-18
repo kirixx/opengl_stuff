a9247ce1cd2c626dd90aba8d8877aedea91e5d5d
Kirill <ksfonmu@gmail.com>
2020-10-18 17:12:39 +0300

    GL buffer init

--- OpenGl_Stuff/src/main.cpp
+++ OpenGl_Stuff/src/main.cpp
@@ -36,18 +36,26 @@ int main()
     if (glewInit() != GLEW_OK)
         std::cout << "GLEW Error" << std::endl;
 
+    float positions[6] = {
+        -0.5f, -0.5f,
+         0.0f,  0.5f,
+         0.5f, -0.5f
+    };
+    uint32_t buffer;
+    glGenBuffers(1, &buffer);
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
     /* Loop until the user closes the window */
     while (!glfwWindowShouldClose(window))
     {
         /* Render Here*/
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.0f,  0.5f);
-        glVertex2f(0.5f, -0.5f);
-        glEnd();
-        
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
         /* Swap front and check buffers */
         glfwSwapBuffers(window);
 
