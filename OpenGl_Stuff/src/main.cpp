44af0eab63e53d84ff3e19fd36ea866135fae8b4
Kirill <ksfonmu@gmail.com>
2020-10-22 14:32:34 +0300

    index buffer first blood

--- OpenGl_Stuff/src/main.cpp
+++ OpenGl_Stuff/src/main.cpp
@@ -113,15 +113,25 @@ int main()
     if (glewInit() != GLEW_OK)
         std::cout << "GLEW Error" << std::endl;
 
-    float positions[6] = {
-        -0.5f, -0.5f,
-         0.0f,  0.5f,
-         0.5f, -0.5f
+    float positions[] = {
+        -0.5f, -0.5f, //0 
+         0.5f, -0.5f, //1
+         0.5f,  0.5f, //2
+        -0.5f,  0.5f, //3
     };
+    
+    uint32_t indices[] = {0, 1, 2,
+                          2, 3, 0};
+
     uint32_t buffer;
     glGenBuffers(1, &buffer);
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
-    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+    glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW);
+    uint32_t ibo;
+    glGenBuffers(1, &ibo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, 6 * sizeof(uint32_t), indices, GL_STATIC_DRAW);
+
     glEnableVertexAttribArray(0);
     glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
 
@@ -135,7 +145,7 @@ int main()
         /* Render Here*/
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glDrawArrays(GL_TRIANGLES, 0, 3);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
         /* Swap front and check buffers */
         glfwSwapBuffers(window);
